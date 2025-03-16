357492070d149d37e5dd4cfe4677c6053955f320
kabirajpan <kabirajpan2@gmail.com>
2025-03-16 14:36:36 +0530

    validation code added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,24 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
 
 const registerUser = asyncHandler( async(req, res) => {
+    //get user details from the data
+    //validation - not epmpty
+    // check if user already exists
+
+    
+    
     const {fullName, username, email, password} = req.body;
     console.log("my email",email)
+
+    // if (fullName === "") {
+    //     throw new ApiError (400, "Full name is required")
+    // }
+
+    if ([fullName, email, username, password].some((field) => field?.trim() === "" )
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
 } )
 
 export {registerUser,}
